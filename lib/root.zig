2a4ceff4179f6917d6102afd745c7cbfea11d7bd
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2025-06-29 15:37:26 +0200

    docs: add file-level license

--- lib/root.zig
+++ lib/root.zig
@@ -1,3 +1,23 @@
+// liburi - URI parsing, production and normalization implementation for the www cyberintern work
+// Copyright (C) 2025 [Cybernetic Internetionale](https://cyberintern.org)
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+// USA
+
+//! URI parsing, production and normalization implementation according to [RFC 3986](https://datatracker.ietf.org/doc/html/rfc3986).
+
 const std = @import("std");
 
 pub const Kind = enum {
